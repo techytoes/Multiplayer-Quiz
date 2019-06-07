4a04f670425300971b8b48600e6086887eba25ce
techytoes <rupeshharode@gmail.com>
2019-06-07 22:24:59 +0530

    Adds Functions to play game

--- game/api.py
+++ game/api.py
@@ -9,20 +9,32 @@ from users.api import validate_user
 import json
 
 
+allowed_users = []
 class GameResource(ModelResource):
     class Meta:
         queryset = Quiz.objects.all()
         quiz_resource = 'quiz'
+        create_resource = 'create'
+        play_resource = 'play'
         allowed_methods = ['get', 'post']
 
     def prepend_urls(self):
         return [
             url(r"^(?P<quiz_resource>%s)/add%s$" %
                 (self._meta.quiz_resource, trailing_slash()),
+                self.wrap_view('add_quiz'), name='api_add_quiz'),
+            url(r"^(?P<create_resource>%s)/game%s$" %
+                (self._meta.create_resource, trailing_slash()),
+                self.wrap_view('create_game'), name='api_create_game'),
+            url(r"^(?P<play_resource>%s)/game%s$" %
+                (self._meta.play_resource, trailing_slash()),
                 self.wrap_view('play_game'), name='api_play_game'),
+            url(r"^(?P<play_resource>%s)/game/submit%s$" %
+                (self._meta.play_resource, trailing_slash()),
+                self.wrap_view('submit_ans'), name='api_submit_ans'),
         ]
 
-    def play_game(self, request, *args, **kwargs):
+    def add_quiz(self, request, *args, **kwargs):
         body = json.loads(request.body)
         username = body.get('username')
         password = body.get('password')
@@ -40,3 +52,44 @@ class GameResource(ModelResource):
 
             # quiz = {created_by : quiz_question}
             return self.create_response(request, {'status': True, 'Message': 'to be decided'})
+
+    # Allow a user to create game
+    def create_game(self, request, *args, **kwargs):
+        body = json.loads(request.body)
+        username = body.get('username')
+        password = body.get('password')
+        allowed_users = body.get('allowed_users')
+
+        if validate_user(username, password):
+            # Returns the link to created Quiz
+            return self.create_response(request, {'status': True, 'Game created at ' : '127.0.0.1:8000/api/v2/play/game/'})
+
+    # Allows user to view quiz
+    def play_game(self, request, *args, **kwargs):
+        body = json.loads(request.body)
+        username = body.get('username')
+        password = body.get('password')
+
+        if validate_user(username, password):
+
+            if username not in allowed_users:
+                return self.create_response(request, {'status':True, 'Message':'User not allowed to view/play the game'})
+
+            else:
+                # Will insert in a dictionary here and create a response once I know how to iterate through quiz.
+
+    # Allows user to submit responses
+    def submit_ans(self, request, *args, **kwargs):
+        body = json.loads(request.body)
+        username = body.get('username')
+        password = body.get('password')
+        responses = body.get('responses')
+
+        if validate_user(username, password):
+
+            if username not in allowed_users:
+                return self.create_response(request, {'status':True, 'Message':'User not allowed to view/play the game'})
+
+            else:
+                # Check the responses with correct option for those questions
+                # And update score accordingly
--- game/models.py
+++ game/models.py
@@ -8,3 +8,6 @@ class Quiz(models.Model):
     quiz_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     question = models.ManyToManyField(Question)
     created_by = models.ForeignKey(Users, on_delete=models.CASCADE)
+
+class Game(models.Model):
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
