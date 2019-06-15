b2c6801d01cac0ba9bbf9152d74b18a4929f8a3b
techytoes <rupeshharode@gmail.com>
2019-06-15 12:40:14 +0530

    Updates condition for allowed users

--- game/utils.py
+++ game/utils.py
@@ -9,9 +9,8 @@ HELPER FUNCTIONS
 # Checks if the user is allowed to participate in the Game
 def is_allowed(creator_user_id, api_key):
 
-    user_name = fetch_id(api_key).name
+    user_name = fetch_id(api_key)
     for game in Game.objects.all():
-        for user in game.allowed_users.all():
-            if user.name == user_name:
-                return True
+        if game.quiz.created_by == creator_user_id and user_name in game.allowed_users.all():
+            return True
     return False
